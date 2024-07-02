7053ce1c4d501d7c3da5b3b9b5ad9efadbd79039
Milan Thaliath <milantomy12@gmail.com>
2024-07-02 11:31:48 -0400

    new change

--- src/main/java/com/example/demo/SpringJenkinsApplication.java
+++ src/main/java/com/example/demo/SpringJenkinsApplication.java
@@ -20,7 +20,7 @@ public class SpringJenkinsApplication {
 	public static void main(String[] args) {
 
 		log.info("Spring Project Executed.................");
-
+		log.info("New change");
 		SpringApplication.run(SpringJenkinsApplication.class, args);
 	}
 
